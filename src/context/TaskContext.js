17b15d0a2da605af03b65dd0a6bcdd30cdcb05da
vipink28 <vipinkalyan28@gmail.com>
2023-09-26 10:29:11 +0530

    all task get function added

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -1,11 +1,15 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
 
 const TaskContext = createContext();
 
 
 export const TaskProvider = ({children})=>{
-    const { message, setMessage } = useContext(AuthContext);
+    const { message, setMessage, user } = useContext(AuthContext);
+    const [allTasks, setAllTasks]=useState(null);
+    const [recentTasks, setRecentTasks]=useState(null);
+    const [latestTask, setLatestTask]=useState(null);
+
     const createTask = async(formData)=>{
         const config = {
             method: "POST",
@@ -22,9 +26,28 @@ export const TaskProvider = ({children})=>{
         }
     }
 
+    const getTasks=async(userid)=>{
+        const response = await fetch(`http://localhost:5000/tasks?userid=${user.id}`, {method: "GET"});
+        const tasks = await response.json();
+        setAllTasks(tasks);
+        const recent = tasks.slice(-3);
+        setRecentTasks(recent);
+        const latest = tasks[tasks.length - 1];
+        setLatestTask(latest);
+    }
+
+    useEffect(()=>{
+        if(user){
+            getTasks();
+        }       
+    }, [user]);
+
     return( 
     <TaskContext.Provider value={{
-        createTask
+        createTask,
+        latestTask,
+        recentTasks,
+        allTasks
     }}>
         {children}
     </TaskContext.Provider>
--- src/pages/CreateTask.jsx
+++ src/pages/CreateTask.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TaskForm from '../components/TaskForm';
+import TaskContext from '../context/TaskContext';
 
 function CreateTask(props) {
+    const {latestTask} = useContext(TaskContext);
+
     return (
         <div className='container-fluid h-100'>
             <div className="row h-100">
@@ -10,11 +13,21 @@ function CreateTask(props) {
                 </div>
 
                 <div className="col-lg-6 h-100 d-flex align-items-center justify-content-center">
-                    <div className="card auth-card">
-                        <div className="card-body">
-                            
+                    
+                    <div className="card bg-primary w-50">
+                        <div className="card-body text-white">
+                            <div className="d-flex">
+                                <h3>{latestTask?.title}</h3>
+                                <button className='btn btn-info ms-auto'>Edit</button>
+                            </div>
+                            <p>{latestTask?.description}</p>
+                            <div className="d-flex">
+                                <p>Modiefied On: {latestTask?.modifiedon}</p>
+                                <p className='ms-auto'>Due On: {latestTask?.duedate}</p>
+                            </div>
                         </div>
                     </div>
+
                 </div>                
             </div>
         </div>
